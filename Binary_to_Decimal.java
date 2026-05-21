ae46f8c929b9e109e9b3cb3df371b661a764bf10
BIKRAM KUNDU <bikramkundu20122005@gmail.com>
2026-05-21 20:46:58 +0530

    Add files via upload

---
+++ Binary_to_Decimal.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Binary_to_Decimal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Input a binary number: ");
+        String binary = sc.nextLine();
+
+        int decimal = Integer.parseInt(binary, 2);
+
+        System.out.println("Decimal Number: " + decimal);
+    }
+    }
+
