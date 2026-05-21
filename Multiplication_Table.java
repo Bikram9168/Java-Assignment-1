e3823404141be3adccecb7148ffcd2ab0c263dc4
BIKRAM KUNDU <bikramkundu20122005@gmail.com>
2026-05-21 20:41:30 +0530

    Add files via upload

---
+++ Multiplication_Table.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Multiplication_Table {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Input a number: ");
+        int n = sc.nextInt();
+
+        for(int i = 1; i <= 10; i++) {
+            System.out.println(n + " x " + i + " = " + (n * i));
+        }
+    }
+}
