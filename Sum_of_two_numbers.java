f438121b00701658e0143fb6fc03b503dd742dc8
BIKRAM KUNDU <bikramkundu20122005@gmail.com>
2026-05-21 20:38:23 +0530

    Add files via upload

---
+++ Sum_of_two_numbers.java
@@ -0,0 +1,10 @@
+public class Sum_of_two_numbers {
+    public static void main(String[] args) {
+        int a = 74;
+        int b = 36;
+
+        int sum = a + b;
+
+        System.out.println("Sum = " + sum);
+    }
+}
